b68f88dbb0290db10db32ecfe9a7e17d6501a532
Jeg27 <114416002+Jeg27@users.noreply.github.com>
2023-06-09 12:04:47 -0400

    Add files via upload

---
+++ gas.bat
@@ -0,0 +1,3 @@
+C:/eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe  -mgasMA.ma -lgasLOG.log -t00:00:05:000 -lgas.log 
+rem 
+pause
---
+++ gasDRW.bat
@@ -0,0 +1,2 @@
+drawlog  -mgasMA.ma -cgas -lgasLOG.log -w3 -p0 -0 > gas.drw
+pause
